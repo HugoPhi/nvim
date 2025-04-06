fc03b47655c671009f57a12c517c1985927d356b
Yunming Hu <hugonelsonm3@gmail.com>
2025-04-06 22:56:36 +0800

    fixed wrap map & quit telescope & add terminal toggle keymap.

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -58,9 +58,9 @@ lspconfig.pylsp.setup {
   },
 }
 
-require("lspconfig").clangd.setup {
-  cmd = { "clangd", "--background-index", "--clang-tidy", "--log=verbose" },
-  init_options = {
-    fallbackFlags = { "-std=c++20" },
-  },
-}
+-- require("lspconfig").clangd.setup {
+--   cmd = { "clangd", "--background-index", "--clang-tidy", "--log=verbose" },
+--   init_options = {
+--     fallbackFlags = { "-std=c++20" },
+--   },
+-- }
